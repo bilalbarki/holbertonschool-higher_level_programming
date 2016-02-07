5acd5f1f06f502f458d7cc62a5a40aab4fecdf2a
bilalbarki <bilalbarki@gmail.com>
2016-02-07 05:29:00 +0500

    Discovering Python

--- discover_python/taxes_and_tip_calculator.py
+++ discover_python/taxes_and_tip_calculator.py
@@ -1,8 +1,10 @@
-#Written by Bilal Ahmad Khan
-#Project: Discover Python (Python 2.7)
-#Taxes and Tip Calculator
+"""
+Written by Bilal Ahmad Khan
+Project: Discover Python (Python 2.7)
+Taxes and Tip Calculator
+"""
 
-print "Welcome to the taxes and tip calculator!" 					#Prints the given string
+print "Welcome to the taxes and tip calculator!"					#Prints the given string
 _priceBeforeTax=float(raw_input("What is the price before tax? "))	#Ask the user to input price before tax, convert it to float and save it to a variable
 _taxes=float(raw_input("What are the taxes? (in %) "))				#Ask the user to input tax in percentage, convert it to float and save it to a variable
 _tip=float(raw_input("What do you want to tip? (in %) "))			#Ask the user to input the tip, convert it to float and save it to a variable
