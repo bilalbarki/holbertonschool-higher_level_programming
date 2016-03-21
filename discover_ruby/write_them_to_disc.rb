59d28af0bbbbbbc3b7c8af6610f0ee8c95dba00e
bilalbarki <bilalbarki@gmail.com>
2016-03-21 10:23:16 +0500

    Changed open to File.open

--- discover_ruby/write_them_to_disc.rb
+++ discover_ruby/write_them_to_disc.rb
@@ -6,7 +6,7 @@ def req(url, query, extheaders)
 end
 
 extheaders = { 'User-Agent' => 'Holberton_School', 'Authorization' => 'token fa51959c07bf48159fbaa3c6800b12efb49f217e' }
-open('/tmp/40', 'w') do |file|
+File.open('/tmp/40', 'w') do |file|
 	file.write( req('https://api.github.com/search/repositories?q=language:ruby&sort=stars&order=desc', nil, extheaders) )
 end
 puts "The file was saved!"
