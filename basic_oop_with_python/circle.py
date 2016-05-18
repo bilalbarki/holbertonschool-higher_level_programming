8ccee14d745e86a8e2f87bf88191eb6cca2b142b
bilalbarki <bilalbarki@gmail.com>
2016-05-18 16:15:34 +0000

    final

--- basic_oop_with_python/circle.py
+++ basic_oop_with_python/circle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-from math import pi
+import math
 
 ''' Class Circle '''
 class Circle():
@@ -22,7 +22,7 @@ class Circle():
         return self.__color
 
     def area(self):
-        return pi*self.__radius*self.__radius
+        return math.pi*self.__radius*self.__radius
 
     def intersection(self, c_bis):
         '''sum of radii'''
@@ -40,3 +40,30 @@ class Circle():
         else:
             return 0
 
+    def intersection_percentage(self, c_bis):
+        '''intersection percentage'''
+        if (self.intersection(c_bis) == 0):
+            return 0
+        radius = self.__radius
+        radius2 = c_bis.__radius
+        RADIUS1 = radius ** 2
+        RADIUS2 = radius2 ** 2
+        XAXIS = c_bis.get_center()[0] - self.get_center()[0]
+        YAXIS = c_bis.get_center()[1] - self.get_center()[1]
+        XYSQUARE = XAXIS ** 2 + YAXIS ** 2
+        distance = math.sqrt(XAXIS ** 2 + YAXIS ** 2)
+        if (distance==0 and radius == 0 and radius2 == 0):
+            return 100
+        elif (radius == 0 and radius2 == 0):
+            return 0;
+        if (radius > radius2 and distance == 0):
+            return RADIUS2/RADIUS1 * 100.0
+        elif (distance == 0):
+            return 100;
+        if (distance + radius2 < radius):
+            return RADIUS2 / RADIUS1 * 100.0
+        elif (distance + radius < radius2):
+            return 100
+        
+        area = RADIUS1 * math.acos((XYSQUARE + RADIUS1 - RADIUS2)/(2 * distance * radius)) + RADIUS2 * math.acos((XYSQUARE + RADIUS2 - RADIUS1)/(2 * distance * radius2)) - (1./2) * math.sqrt((-distance+radius+radius2)*(distance+radius-radius2)*(distance-radius+radius2)*(distance+radius+radius2))
+        return  area / (math.pi * RADIUS1) * 100
