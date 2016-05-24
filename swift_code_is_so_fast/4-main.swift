1785b4f283a254ab710427a6540b5d373a258459
bilalbarki <bilalbarki@gmail.com>
2016-05-24 14:37:51 +0100

    added new

--- swift_code_is_so_fast/4-main.swift
+++ swift_code_is_so_fast/4-main.swift
@@ -1,6 +1,6 @@
 
 var numbers = [4, 7, 1, 9, 6, 5, 6, 9]
 
-let max = Int(numbers.maxElement())
+let max = numbers.sort(){$0 > $1}[0]
 
 print(max)
---
+++ swift_code_is_so_fast/8-main.swift
@@ -0,0 +1,12 @@
+
+let strings = ["We", "Heart", "Swift"]
+
+let string = strings.reduce("") {
+    if $0 == "" {
+        return $1
+    } else {
+        return $0 + " " + $1
+    }
+}
+
+print(string)
