d39760bdeec9a1d240c81f6168054daebfdd813c
bilalbarki <bilalbarki@gmail.com>
2016-05-31 02:17:58 +0000

    minor fixes

--- hodor/level_0/script_threading.py
+++ hodor/level_0/script_threading.py
@@ -7,12 +7,14 @@ def thread_it(i):
     urllib2.urlopen('http://173.246.108.142/level0.php', url_params)
 
 threads = []
+print "Please standby..."
+
 for i in range (1, 1025):
     t = threading.Thread(target=thread_it,args=(i,))
     threads.append(t)
     t.start()
     print "Vote", i, "has been submitted for processing!"
     time.sleep(0.3)
-print "Please standby..."
+
 t.join()
 print "Done!"
