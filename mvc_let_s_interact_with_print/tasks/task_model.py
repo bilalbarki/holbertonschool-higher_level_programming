5ad90e2d6c6a4aca0b289c63644cdcb750b0a58c
bilalbarki <bilalbarki@gmail.com>
2016-05-31 12:54:50 +0500

    time over

--- mvc_let_s_interact_with_print/tasks/task_model.py
+++ mvc_let_s_interact_with_print/tasks/task_model.py
@@ -1,23 +1,23 @@
 class TaskModel():
     '''initializaton'''
-    def __init__(self, title):
-        if isinstance(title,str) and title!="":
-            self.__title=title
+    def __init__(self):
+            self.__title=[]
             self.__callback_title = None
-        else:
-            raise Exception("title is not a string")
 
     '''getter for title'''
     def get_title(self):
         return self.__title
+
+    def set_title(self,title):
+        self.__title.append(title)
 
     '''setter for callback_title'''
     def set_callback_title(self, value):
         self.__callback_title = value
 
     '''reverses title and if callback_title is set, calls it with titl as argument'''
-    def toggle(self):
-        self.__title = self.__title[::-1]
+    def toggle(self,  index):
+        self.__title[index] = self.__title[index][::-1]
         if self.__callback_title:
             self.__callback_title(self.__title)
 
